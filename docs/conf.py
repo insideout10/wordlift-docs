44fba90108e337f2f1232c4c918cb6820f0359b9
David Riccitelli <david@insideout.io>
2014-05-27 10:19:54 +0300

    fix copyright

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'WordLift'
-copyright = u'2014, <a href="http://insideout.io">InsideOut10</a>.'
+copyright = u'2014, InsideOut10.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
