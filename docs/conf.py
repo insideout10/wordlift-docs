6f9094be8294bb749be6111d2944385f43932ff0
Andrea Volpini <andrea@wordlift.io>
2019-09-20 10:04:07 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = ['sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+html_theme_path = ["_themes", ]
 
 # The suffix of source filenames.
 source_suffix = '.rst'
